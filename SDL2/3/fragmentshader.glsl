642858ca105629c05e982ae14307b3421a93df41
Jordy <jordyfgomes@gmail.com>
2014-11-08 12:27:45 -0300

    Mudando a cor dos triangulos pelo shader!

--- SDL2/3/fragmentshader.glsl
+++ SDL2/3/fragmentshader.glsl
@@ -3,5 +3,7 @@ uniform vec3 triangleColor;
 out vec4 outColor;
 
 void main(){
-	outColor = vec4(triangleColor, 1.0);
+	float gray = (triangleColor.r + triangleColor.g + triangleColor.b)/3.0;
+	vec3 grayShade = vec3(gray,gray,gray);	
+	outColor = vec4(grayShade, 1.0);
 }
