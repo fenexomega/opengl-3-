9cd79b8ec2600ba28d66ad2a2f02603cd3ef742e
Jordy <jordyfgomes@gmail.com>
2014-12-03 10:01:41 -0300

    Transformações! UHU!

---
+++ SDL2/1-7/build/CMakeFiles/1-7.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1-7.dir/src/DisplaySDL2.cpp.o"
+  "CMakeFiles/1-7.dir/src/main.cpp.o"
+  "CMakeFiles/1-7.dir/src/DisplaySFML.cpp.o"
+  "1-7.pdb"
+  "1-7"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1-7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
